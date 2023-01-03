8b5f85f460485abf924f512f7ec70ef97e720636
muhammad <abduvaliyevmuhammad41@gmail.com>
2023-01-03 21:21:08 +0500

    add

--- leetcode/944. Delete Columns to Make Sorted.py
+++ leetcode/944. Delete Columns to Make Sorted.py
@@ -1,15 +1,6 @@
-def is_sorted(l):
-    for i in range(1,len(l)):
-        if l[i-1] > l[i]:
-            return False
-    return True
 def minDeletionSize(strs: list[str]) -> int:
-    strs = [[j for j in i] for i in strs]
     c = 0
     for i in zip(*strs):
-        if not is_sorted(''.join(i)):
+        if list(i) != sorted(i):
             c += 1
     return c
-
-
-print(minDeletionSize(['salom', 'salom']))
