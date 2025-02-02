877d2e29e7c24179de4c5edce7e73af907c4e7c8
Muhammad <99497268+IaMuhammad@users.noreply.github.com>
2025-02-03 00:20:11 +0500

    Update insertion_sort.py

--- algorithm/insertion_sort.py
+++ algorithm/insertion_sort.py
@@ -1,14 +1,12 @@
-def insertion_sort_fe(arr):
-    l = len(arr)
-    for i in range(1,l):
-        for j in range(i-1,-1,-1):
-            if arr[i] >= arr[j]:
-                x = arr.pop(i)
-                
-                arr.insert(j+1, x)
+def sortArray(nums):
+    for i in range(1, len(nums)):
+        for j in range(i, -1, -1):
+            if  j and nums[j] < nums[j-1]:
+                x = nums.pop(j)
+                nums.insert(j-1, x)
+            else:
                 break
-
-    return arr
+    return nums
 
 def insertion_sort_we(arr):
     i, l = 1, len(arr)
