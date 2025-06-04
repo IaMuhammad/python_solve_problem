de87042cd7f4bf9e994de0a84c69075dfb406764
muhammad <abduvaliyevmuhammad41@gmail.com>
2025-06-04 13:58:34 +0500

    Implement solutions for "Merge Sorted Array" and "Two Sum II - Input Array Is Sorted"

--- leetcode/167. Two Sum II - Input Array Is Sorted.py
+++ leetcode/167. Two Sum II - Input Array Is Sorted.py
@@ -1,22 +1,14 @@
+from typing import List
+
+
 class Solution:
-    def binary_search(self, arr: list, target: int, l: int):
-        r = len(arr)-1
-        while l <= r:
-            m = (l + r) // 2
-            if arr[m] == target:
-                return m + 1
-            elif arr[m] > target:
-                r = m - 1
-            else:
-                l = m + 1
-        return 0
-
-    def twoSum(self, numbers: list[int], target: int) -> list[int]:
-        for i, v in enumerate(numbers):
-            b = self.binary_search(numbers, target - v, i + 1)
-            if b:
-                return [i+1, b]
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        data = {}
+        for i, v in enumerate(numbers, 1):
+            if data.get(target - v):
+                return [data.get(target - v), i]
+            data[v] = i
 
 
-a = Solution()
-print(a.twoSum([5,25,75],100))
+a = Solution().twoSum([2, 7, 11, 15], 9)
+print(a)  # Output: [1, 2]
--- leetcode/88. Merge Sorted Array.py
+++ leetcode/88. Merge Sorted Array.py
@@ -3,5 +3,16 @@ from typing import List
 
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-
-        pass
+        _nums1 = nums1[:m]
+        i = 0
+        while _nums1 and nums2:
+            if _nums1[0] <= nums2[0]:
+                nums1[i] = _nums1.pop(0)
+            elif _nums1[0] > nums2[0]:
+                nums1[i] = nums2.pop(0)
+            else:
+                nums1[i] = nums2.pop(0)
+            i += 1
+        nums1[i:] = _nums1 + nums2
+        print(nums1)
+a = Solution().merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)
